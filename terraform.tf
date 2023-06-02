48d3116e2d9f40c61c48540d52fc413c9c022cd3
rita <8647768+ritsok@users.noreply.github.com>
2023-06-02 13:52:11 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,7 @@
 terraform {
   /* Uncomment this block to use Terraform Cloud for this tutorial
   cloud {
+    organization = "organization-name"
     workspaces {
       name = "learn-terraform-checks"
     }
